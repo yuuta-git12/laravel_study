9d434a0bd908313eedb9b49f750b6e411919c935
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2023-10-11 01:40:49 +0900

    [create]１章用のファイル作成

---
+++ html/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTG-8">
+        <meta name="viewpoint" content="width-device-width, initial-scale=1.0">
+        <title>PHPの基本</title>
+    </head>
+    <body>
+        <?php
+        print "こんにちは、世界<br/>";
+        print "こんにちは、皆さん!<br/>";
+        $value = "頑張ろう、PHP!";
+        print_r($value);
+        ?>
+    </body>
+</html>
